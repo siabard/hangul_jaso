3ecce2aa44dd2db7f09c2a23a0d52c9b7b5cebd1
yhjang <siabard@gmail.com>
2022-09-10 18:09:00 +0900

    한글 8x4x4 벌 계산

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,17 @@
 /// 초성, 중성, 종성, 8x4x4 체계에서의 각 벌을 구하는 프로그램
 use hangul_jaso::*;
 
-fn main() {
-    let code = utf8_to_ucs2(&"얹").unwrap();
+fn print_jaso_bul(t: &dyn ToString) {
+    let code = utf8_to_ucs2(t).unwrap();
+    let jaso = build_jaso(code).unwrap();
+    let bul = build_bul(jaso);
 
-    // 0b1010110000000000
-    println!("{:#0b} {:?}", code, build_jaso(code).unwrap());
+    println!("{:#0x} {:?} {:?}", code, jaso, bul)
+}
+
+fn main() {
+    print_jaso_bul(&"가");
+    print_jaso_bul(&"각");
+    print_jaso_bul(&"고");
+    print_jaso_bul(&"구");
 }
