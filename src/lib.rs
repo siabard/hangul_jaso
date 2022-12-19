d81646aa33e8fe8ea2811cada199b1a610a062ae
Yeonho Jang <siabard@gmail.com>
2022-12-20 08:25:42 +0900

    일부 주석 정리

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,11 @@
-/// 초성 ㄱㄲㄴㄷㄸㄹㅁㅂㅃㅅㅆㅇㅈㅉㅊㅋㅌㅍㅎ
-/// 중성 ㅏㅐㅑㅒㅓㅔㅕㅖㅗㅘㅙㅚㅛㅜㅝㅞㅟㅠㅡㅢㅣ
-/// 종성 ㄱㄲ(ㄱㅅ)ㄴ(ㄴㅈ)(ㄴㅎ)ㄷㄹ(ㄹㄱ)(ㄹㅁ)(ㄹㅂ)(ㄹㅅ)(ㄹㅌ)(ㄹㅍ)(ㄹㅎ)ㅁㅂ(ㅂㅅ)ㅅㅆㅇㅈㅊㅋㅌㅍㅎ
+//! utf8 문자열이나 ucs2 문자열중 한글을 자소로 분리하는 라이브러리
+//!
+//! 지원하는 한글 자소는 아래와 같다.
+//! - 초성 ㄱㄲㄴㄷㄸㄹㅁㅂㅃㅅㅆㅇㅈㅉㅊㅋㅌㅍㅎ
+//! - 중성 ㅏㅐㅑㅒㅓㅔㅕㅖㅗㅘㅙㅚㅛㅜㅝㅞㅟㅠㅡㅢㅣ
+//! - 종성 ㄱㄲ(ㄱㅅ)ㄴ(ㄴㅈ)(ㄴㅎ)ㄷㄹ(ㄹㄱ)(ㄹㅁ)(ㄹㅂ)(ㄹㅅ)(ㄹㅌ)(ㄹㅍ)(ㄹㅎ)ㅁㅂ(ㅂㅅ)ㅅㅆㅇㅈㅊㅋㅌㅍㅎ
 
+/// ucs-2 코드 값으로 확인되는 해당 문자열의 언어
 #[derive(PartialEq, Eq, Hash)]
 pub enum Languages {
     Ascii,
@@ -12,6 +16,19 @@ pub enum Languages {
     NotImplemented,
 }
 
+/// ucs2 코드로 해당 언어를 반환한다.
+pub fn ucs2_language(code: u16) -> Languages {
+    match code {
+        0x0000..=0x007f => Languages::Ascii,
+        0xac00..=0xd7a3 => Languages::Hangul,
+        0x3131..=0x3163 => Languages::HangulJamo,
+        0x3040..=0x30ff => Languages::Kana,
+        0x2190..=0x2199 => Languages::Arrow,
+        _ => Languages::NotImplemented,
+    }
+}
+
+/// 한글의 초, 중, 종성 값을 보관하는 구조체
 #[derive(Default, Debug, Copy, Clone)]
 pub struct Jaso {
     pub cho: u8,
@@ -19,6 +36,9 @@ pub struct Jaso {
     pub jong: u8,
 }
 
+/// 각 자소가 어떤 벌 값인지 알려준다.
+/// 이 라이브러리에서는 8x4x4 한글 자소 폰트 기반으로
+/// 구성하는 것을 전제로 한다.
 #[derive(Default, Debug, Copy, Clone)]
 pub struct Bul {
     pub cho: Option<u8>,
@@ -26,10 +46,12 @@ pub struct Bul {
     pub jong: Option<u8>,
 }
 
+/// 종성의 갯수. 현대 (2022 기준) 사용되는 종성의 수는 28개이다.
 pub const NUM_OF_JONG: u16 = 28;
+/// 중성의 갯수. 현대 (2022 기준) 사용되는 중성의 수는 21개이다.
 pub const NUM_OF_MID: u16 = 21;
 
-/// UTF8로 표시된 (1~4바이트) 글자를 16비트(2바이트) UCS2 값으로 전환하기
+/// UTF8로 표시된 (1~4바이트) 글자를 16비트(2바이트) UCS2 값으로 전환
 pub fn utf8_to_ucs2(s: &dyn ToString) -> Result<u16, String> {
     let str = s.to_string();
     let raw_bytes = str.as_bytes();
@@ -73,23 +95,11 @@ pub fn utf8_to_ucs2(s: &dyn ToString) -> Result<u16, String> {
     Ok(result)
 }
 
-pub fn ucs2_language(code: u16) -> Languages {
-    match code {
-        0x0000..=0x007f => Languages::Ascii,
-        0xac00..=0xd7a3 => Languages::Hangul,
-        0x3131..=0x3163 => Languages::HangulJamo,
-        0x3040..=0x30ff => Languages::Kana,
-        0x2190..=0x2199 => Languages::Arrow,
-        _ => Languages::NotImplemented,
-    }
-}
-
 /// 초성, 중성, 조성의 값을 가져오기
 /// 유니코드에서 완성형 한글은 ac00(가)~d7a3(힣)까지 초성, 중성, 종성을 순서대로 조합해 배열한 것이다.
 /// 완성형 한글 코드 = (((초성번호 * 중성개수) + 중성번호) * 종성개수) + 종성번호 + ac00
 /// 중성갯수 : 21개
 /// 종성갯수 : 28개
-
 pub fn build_jaso(code: u16) -> Result<Jaso, String> {
     // MLB (가장 좌측 비트가 1인지 검사)
     if (code & 0b1000_0000_0000_0000) == 0b1000_0000_0000_0000 {
@@ -177,12 +187,23 @@ pub fn build_bul(jaso: &Jaso) -> Bul {
     Bul { cho, mid, jong }
 }
 
+/// 해당 문자열 코드로부터 자소와 벌 값을 한번에 가져오기
+pub fn build_jaso_bul(t: &dyn ToString) -> (Jaso, Bul) {
+    let code = utf8_to_ucs2(t).unwrap();
+    let jaso = build_jaso(code).unwrap();
+    let bul = build_bul(&jaso);
+
+    (jaso, bul)
+}
+
+/// u16 값(ucs-2)을 자소로 변환하는 유틸리티
 impl From<u16> for Jaso {
     fn from(code: u16) -> Self {
         build_jaso(code).unwrap()
     }
 }
 
+/// Jaso 값을 토대로 Bul을 반환하는 유틸리티
 impl From<&Jaso> for Bul {
     fn from(jaso: &Jaso) -> Self {
         build_bul(jaso)
